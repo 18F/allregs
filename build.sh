6f31765a1898ccfe4146ee50848f519df4bf3e7c
Anthony Garvan <anthony.garvan@gsa.gov>
2016-07-15 22:23:39 -0500

    titles 1-5 again

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@ mkdir -p data/text
 mkdir -p html/sections
 mkdir -p html/titles
 mkdir -p html/parts
+npm install
+cp node_modules/uswds/dist/css/uswds.min.css css
 python get_cfr_text.py
 python parse_structure.py --debug
