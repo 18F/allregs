0f10affa243f1d129291dfe5109f791d42ec4920
Anthony Garvan <anthony.garvan@gmail.c>
2016-07-18 13:29:18 -0400

    all regs parsed with higher accuracy

--- parallel_run.sh
+++ parallel_run.sh
@@ -1,7 +1,7 @@
 for i in `seq 1 50`; do
   echo starting title $i
-  python parse_structure.py --title-start=$i --title-end=$i --debug && echo finished title $i &
-  sleep 5
+  python parse_structure.py --title-start=$i --title-end=$i && echo finished title $i &
+  sleep 2
 done
 wait
 python parse_structure.py --title-only
